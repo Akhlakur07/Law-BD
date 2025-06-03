c0a120eb9071183716cdd937508bea3c96cb4ab8
Akhlakur Rahman <ssrmm2000@gmail.com>
2025-06-03 17:00:04 +0600

    Banner created

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router';
 import App from '../App';
 import Error from '../pages/Error/Error';
+import Home from '../pages/Home/Home';
 
 export const router = createBrowserRouter([
     {
         path: "/",
         Component: App,
-        errorElement: <Error></Error>
+        errorElement: <Error></Error>,
+        children: [
+            {
+                index: true,
+                Component: Home
+            }
+        ]
     }
 ])
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import bannerimg from "../../assets/banner-img-1.png";
+
+const Banner = () => {
+  return (
+    <div>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: `url(${bannerimg})`,
+        }}
+      >
+        <div className="text-center text-neutral-content">
+          <h1 className="mb-5 text-5xl font-extrabold leading-16 tracking-wide w-3/6 mx-auto">
+            It avoids subjective claims or exaggeration that might raise red
+            flags legally
+          </h1>
+          <p className="text-base font-medium w-3/5 mx-auto">
+            Our platform connects you with verified, experienced doctors across
+            various specialties — all at your convenience. Whether it's a
+            routine checkup or urgent consultation, book appointments in minutes
+            and receive quality care you can trust.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
