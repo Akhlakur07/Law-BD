e395155b9d424ef06637d5172e342c2189a3f515
Akhlakur Rahman <ssrmm2000@gmail.com>
2025-06-17 18:39:06 +0600

    blogs section updated

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -28,18 +28,18 @@ const Navbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/bookings">My-Bookings</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact Us</Link>
-          </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/bookings">My-Bookings</Link>
+            </li>
+            <li>
+              <Link to="/blogs">Blogs</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
           </ul>
         </div>
         <div className="flex items-center">
@@ -64,7 +64,11 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="navbar-end">
-        <button className="btn btn-xl bg-[#0EA106] rounded-4xl text-white">Contact Now</button>
+        <Link to="/contact">
+          <button className="btn btn-xl bg-[#0EA106] rounded-4xl text-white">
+            Contact Now
+          </button>
+        </Link>
       </div>
     </div>
   );
